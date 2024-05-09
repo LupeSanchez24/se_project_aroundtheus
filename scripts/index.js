7fc549f5f48861547d89059a652aa42b5c0889b6
GuadalupeSanchez <guadalupesanchezp6@gmail.com>
2024-05-09 10:21:21 -0700

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -72,6 +72,22 @@ function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
   const cardTitleEl = cardElement.querySelector(".card__title");
+  const likeButton = cardElement.querySelector(".card__like-button");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  //find delete button
+
+  //add the event listener to the delete button
+  //add cardElement.remove();when this button gets click
+  deleteButton.addEventListener("click", () => {
+    cardElement.remove("card__delete-button_active");
+  });
+
+  likeButton.addEventListener("click", () => {
+    likeButton.classList.toggle("card__like-button_active");
+  });
+  //add click listener to the cardImage element
+  //openModal with previewImageModal "add previewImageModal to HTMl"
+
   cardImageEl.src = cardData.link;
   cardImageEl.alt = cardData.name;
   cardTitleEl.textContent = cardData.name;
