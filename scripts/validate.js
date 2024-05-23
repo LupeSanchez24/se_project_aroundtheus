9c4e59912aca23bb2da2fff77c2a0a1c70df75c9
GuadalupeSanchez <guadalupesanchezp6@gmail.com>
2024-05-23 13:51:26 -0700

    New update for my project

---
+++ scripts/validate.js
@@ -0,0 +1,45 @@
+function showInputError() {}
+
+function checkInputValidity() {}
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      console.log(inputEl.validity.valid);
+    });
+  });
+}
+
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (e) => {
+      e.preventDefault();
+    });
+
+    setEventListeners(formEl, options);
+    // look for all inputs inside of form
+    // loop thorugh all the inputs to see if all are valid
+    // if input is not valid
+    // get validation message
+    // add error class to input
+    //display error message
+    // disable button
+    //if all inputs are valid
+    // enable button
+    // reset error messages
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+enableValidation(config);
